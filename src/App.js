801b9ffe0356e67b4369af0be79ff87afc1df690
gouz7514 <gouz7514@gmail.com>
2020-10-23 19:35:38 +0900

    Component Practice

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+function Food({fav}) {
+  return <h3>I Love {fav}</h3>
+}
+
 function App() {
   return (
     <div className="App">
       <h1>HELLO</h1>
+      <Food fav="kimchi" />
+      <Food fav="you" />
     </div>
   );
 }
