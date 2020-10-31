d3320bfa434c686077e34e265b640ef6cedad154
gouz7514 <gouz7514@gmail.com>
2020-10-31 14:23:30 +0900

    Updates

---
+++ precache-manifest.f120cca0ee500380528a55088764121f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5229701bfc06759dc26f3bbe7dcb0cf",
+    "url": "/movie_app_khj/index.html"
+  },
+  {
+    "revision": "cd41dce9edce2a576312",
+    "url": "/movie_app_khj/static/css/main.2b34dded.chunk.css"
+  },
+  {
+    "revision": "2dbbfcf4273a42afe746",
+    "url": "/movie_app_khj/static/js/2.e0aa6413.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/movie_app_khj/static/js/2.e0aa6413.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd41dce9edce2a576312",
+    "url": "/movie_app_khj/static/js/main.5c40f00f.chunk.js"
+  },
+  {
+    "revision": "6bb231d0e1fdb766a0fd",
+    "url": "/movie_app_khj/static/js/runtime-main.d25ed4e4.js"
+  }
+]);
